d9961fc144217399da8424ff03a0582d7c352309
Hazel Lakshmana <hazellakshmana684@gmail.com>
2026-09-09 10:47:38 +0530

    Update rateLimiter.js

--- Hazel_Lakshmana_150096725155/middleware/rateLimiter.js
+++ Hazel_Lakshmana_150096725155/middleware/rateLimiter.js
@@ -1,6 +1,17 @@
-const rateLimit=require("express-rate-limit");
-const bookingRateLimiter=rateLimit({
- windowMs:60000,limit:10,standardHeaders:"draft-8",legacyHeaders:false,
- message:{success:false,message:"Too many booking requests. Please try again later."}
+const rateLimit = require("express-rate-limit");
+
+const bookingRateLimiter = rateLimit({
+    windowMs: 60000,
+    limit: 10,
+    standardHeaders: "draft-8",
+    legacyHeaders: false,
+
+    message: {
+        success: false,
+        message: "Too many booking requests. Please try again later."
+    }
 });
-module.exports={bookingRateLimiter};
+
+module.exports = {
+    bookingRateLimiter
+};
